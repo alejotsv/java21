ed4bc3c4585cf643ce784080ad2fa202c58648a9
Alejandro Salas <asalas.wd@outlook.com>
2020-04-26 00:00:22 -0400

    Create getters and setters, and manipulate spirit robot

--- src/MarsApplication.java
+++ src/MarsApplication.java
@@ -1,5 +1,19 @@
 class MarsApplication {
     public static void main(String[] args) {
-        System.out.println("Hello, Mars Robot!");
+        MarsRobot spirit = new MarsRobot();
+        spirit.setStatus("exploring");
+        spirit.setSpeed(2);
+        spirit.setTemperature(-60);
+
+        spirit.showAttributes();
+
+        while(spirit.getTemperature()>-90){
+            System.out.println("Decreasing temperature by 10 degrees");
+            spirit.temperature -= 10;
+            spirit.checkTemperature();
+            spirit.showAttributes();
+            System.out.println("---------------------------");
+            }
+
     }
 }
--- src/MarsRobot.java
+++ src/MarsRobot.java
@@ -6,15 +6,39 @@ public class MarsRobot {
 
     void checkTemperature() {
         if (temperature < -80) {
-            status = "returning home";
-            speed = 5;
+            setStatus("returning home");
+            setSpeed(5);
         }
     }
 
     void showAttributes() {
-        System.out.println("Status: " + status);
-        System.out.println("Speed: " + speed);
-        System.out.println("Temperature: " + temperature);
+        System.out.println("Status: " + getStatus());
+        System.out.println("Speed: " + getSpeed());
+        System.out.println("Temperature: " + getTemperature());
     }
 
+//    Getters and setters
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(float temperature) {
+        this.temperature = temperature;
+    }
 }
