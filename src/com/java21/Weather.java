92af223c49be3dbdb1650ac3d64d99db3cde75f2
Alejandro Salas <asalas.wd@outlook.com>
2020-04-26 12:22:10 -0400

    Create covert to Celsius method

--- src/com/java21/Weather.java
+++ src/com/java21/Weather.java
@@ -1,4 +1,26 @@
 package com.java21;
 
 public class Weather {
+    public static void main(String[] args){
+        float fahrenheit = 86;
+        showCelsius(fahrenheit);
+
+    }
+
+
+    static float convertToCelsius(float fahrenheit){
+        float celsius;
+        celsius = ((fahrenheit - 32)/9)*5;
+        return celsius;
+    }
+
+    static void showCelsius(float fahrenheit){
+        float celsius = convertToCelsius(fahrenheit);
+        System.out.println(fahrenheit + " degrees Fahrenheit is " + celsius + " degrees Celsius.");
+    }
+
+
+
+
+
 }
