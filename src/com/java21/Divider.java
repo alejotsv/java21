096b75bc6ce7fd63a5f09f9ef3e088a568413ec1
Alejandro Salas <asalas.wd@outlook.com>
2020-04-26 14:36:51 -0400

    Complete methods to display result and remainder

--- src/com/java21/Divider.java
+++ src/com/java21/Divider.java
@@ -2,7 +2,21 @@ package com.java21;
 
 public class Divider {
     public static void main(String[] args){
-        System.out.println("Divide");
+        showResult(40, 20);
+    }
+
+    static int divideNum(int x, int y){
+        return x/y;
+    }
+
+    static int remainder(int x, int y){
+        return x%y;
+    }
+
+    static void showResult(int x, int y){
+        int result = divideNum(x,y);
+        int remainder = remainder(x,y);
+        System.out.println("Result: " + result + "\tRemainder: " + remainder);
     }
 
 }
