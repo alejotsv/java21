9a79590f245133d2ce4e38eb40c20cb32ba95cf2
Alejandro Salas <asalas.wd@outlook.com>
2020-04-27 21:30:03 -0400

    Create birthdaySlicer method

--- src/com/java21/Birthday.java
+++ src/com/java21/Birthday.java
@@ -1,7 +1,22 @@
 package com.java21;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public class Birthday {
-    public static void main(String[] args) {
-        System.out.println("Hello, bird");
+    public static void main(String[] args){
+        System.out.println("Birthday");
+        birthdaySlicer("09/19/81");
     }
+
+    static void birthdaySlicer(String birthday){
+        String[] arr = new String[3];
+        StringTokenizer token = new StringTokenizer(birthday, "/");
+        while(token.hasMoreTokens()){
+            System.out.println(token.nextToken());
+        }
+    }
+
+
 }
