dbac02fe70fb01f508a6911ad1f9d70d694adeed
Alejandro Salas <asalas.wd@outlook.com>
2020-04-28 21:59:51 -0400

    Create populateMonths method

--- src/com/java21/DayCounter.java
+++ src/com/java21/DayCounter.java
@@ -3,7 +3,8 @@ package com.java21;
 public class DayCounter {
     public static void main(String[] args){
         System.out.println("counting days...");
-
+        Object[][] thisYear = populateMonths(1984);
+        System.out.println(thisYear[0][22]);
     }
 
     public static boolean isLeap(int year){
@@ -16,6 +17,12 @@ public class DayCounter {
             }
         }
         return leap;
+    }
+
+    public static Object[][] populateMonths(int year){
+        boolean leap = isLeap(year);
+        Object[][] populatedYear = new Object[12][31];
+        return populatedYear;
     }
 
 }
