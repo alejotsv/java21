b7284596fa8b97bfb7af647abb7398f72047a1ab
Alejandro Salas <asalas.wd@outlook.com>
2020-04-28 21:37:44 -0400

    Create isLeap method

--- src/com/java21/DayCounter.java
+++ src/com/java21/DayCounter.java
@@ -3,5 +3,19 @@ package com.java21;
 public class DayCounter {
     public static void main(String[] args){
         System.out.println("counting days...");
+
     }
+
+    public static boolean isLeap(int year){
+        boolean leap = false;
+        if (year%4 == 0){
+            if (year%100 == 0 && year%400 != 0){
+                return leap;
+            } else {
+                leap = true;
+            }
+        }
+        return leap;
+    }
+
 }
