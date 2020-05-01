02694a04fc5d5a2c61647e9e13d606c161ce00c4
Alejandro Salas <asalas.wd@outlook.com>
2020-04-30 21:47:20 -0400

    Add static class variable for starting date and method to calculate how many days Jan 1 moves

--- src/com/java21/DayCounter.java
+++ src/com/java21/DayCounter.java
@@ -1,11 +1,21 @@
 package com.java21;
 
+import java.sql.Date;
+
 public class DayCounter {
+//    Start day: January 1st, 2020 = Wednesday
+    static int START_DAY = 3;
+    static int START_YEAR = 2020;
+
     public static void main(String[] args){
         System.out.println("counting days...");
         DayCounter myYear = new DayCounter();
         boolean leapYear = myYear.isLeap(2020);
         System.out.println(leapYear);
+
+        System.out.println(myYear.dayChange(2019));
+
+
 
     }
 
@@ -19,6 +29,28 @@ public class DayCounter {
             }
         }
         return leap;
+    }
+
+    public int dayChange(int year){
+        int dayChanger = 0;
+        if (year > START_YEAR){
+            for (int i = year; i > START_YEAR; i--){
+                if(isLeap(i)){
+                    dayChanger += 2;
+                } else {
+                    dayChanger++;
+                }
+            }
+        } else {
+            for (int i = year; i < START_YEAR; i++){
+                if(isLeap(i)){
+                    dayChanger -= 2;
+                } else {
+                    dayChanger--;
+                }
+            }
+        }
+        return dayChanger%7;
     }
 
 
