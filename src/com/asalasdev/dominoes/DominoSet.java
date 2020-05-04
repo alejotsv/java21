61c1af743fdd0d15750ded1279477f1b81b2b1ae
Alejandro Salas <asalas.wd@outlook.com>
2020-05-03 22:01:46 -0400

    Complete domino set creator method

--- src/com/asalasdev/dominoes/DominoSet.java
+++ src/com/asalasdev/dominoes/DominoSet.java
@@ -6,6 +6,10 @@ public class DominoSet {
     int maxValue;
     int numOfPieces;
 
+    public static void main(String[] args){
+        createSet(9, 55);
+    }
+
     public DominoSet(String variant){
         this.variant = variant;
         if (variant == "Venezuelan"){
@@ -18,8 +22,16 @@ public class DominoSet {
 
     }
 
-    Domino[] createSet(int maxValue, int numOfPieces) {
+    static Domino[] createSet(int maxValue, int numOfPieces) {
         Domino[] dominoSet = new Domino[numOfPieces];
+        for (int i=0; i<=maxValue; i++){
+            for (int j=0; j<=maxValue; j++){
+                Domino domino = new Domino(i,j);
+                System.out.println("Pieza: " + domino.top + "|" + domino.bottom);
+            }
+        }
+
+        return dominoSet;
     }
 
 //  Getters
