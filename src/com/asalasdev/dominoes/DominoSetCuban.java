6174e56d2799ed51867874b186b6b53695eea973
Alejandro Salas <asalas.wd@outlook.com>
2020-05-04 21:36:31 -0400

    Complete Cuban class

--- src/com/asalasdev/dominoes/DominoSetCuban.java
+++ src/com/asalasdev/dominoes/DominoSetCuban.java
@@ -1,4 +1,18 @@
 package com.asalasdev.dominoes;
 
-public class DominoSetCuban {
+public class DominoSetCuban extends DominoSet {
+    static final int MAX_VALUE = 9;
+    static final int NUM_OF_PIECES = 55;
+
+    public DominoSetCuban(){
+        super(MAX_VALUE, NUM_OF_PIECES);
+    }
+
+    public static int getMaxValue() {
+        return MAX_VALUE;
+    }
+
+    public static int getNumOfPieces() {
+        return NUM_OF_PIECES;
+    }
 }
--- src/com/asalasdev/dominoes/Game.java
+++ src/com/asalasdev/dominoes/Game.java
@@ -1,11 +1,19 @@
 package com.asalasdev.dominoes;
 
 public class Game {
-    String variant;
 
     public static void main(String[] args){
+        String variant;
         System.out.println("Creating a new game");
-        DominoSetVenezuelan newGame = new DominoSetVenezuelan();
+        variant = "Venezuelan";
+        if (variant.equals("Venezuelan")){
+            DominoSetVenezuelan newGame = new DominoSetVenezuelan();
+        } else if (variant.equals("Cuban")){
+            DominoSetCuban newGame = new DominoSetCuban();
+        } else {
+            System.out.println("The variant " + args[0] + " is not supported.");
+        }
+
     }
 
 }
