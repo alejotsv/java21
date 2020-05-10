5e04b7c377f9781d5aaed8fadf2a59e661c39f05
Alejandro Salas <asalas.wd@outlook.com>
2020-05-10 14:00:11 -0400

    Create SwingExample class

---
+++ src/com/java21/SwingExample.java
@@ -0,0 +1,15 @@
+package com.java21;
+
+import javax.swing.*;
+
+public class SwingExample extends JFrame {
+    public static void main(String[] args) {
+        SwingExample thisExample = new SwingExample();
+    }
+
+    public SwingExample() {
+        super("Swing Example");
+        setSize(600, 600);
+        setVisible(true);
+    }
+}
