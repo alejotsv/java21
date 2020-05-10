58eb51ce1a1ab716b95e799c9b71a481b013846f
Alejandro Salas <asalas.wd@outlook.com>
2020-05-10 14:40:47 -0400

    Get all installed look and feels and store them in String ArrayList

--- src/com/java21/SwingExample.java
+++ src/com/java21/SwingExample.java
@@ -1,10 +1,13 @@
 package com.java21;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SwingExample extends JFrame {
     public static void main(String[] args) {
         SwingExample thisExample = new SwingExample();
+        System.out.println(UIManager.getLookAndFeel());
     }
 
     public SwingExample() {
@@ -12,5 +15,16 @@ public class SwingExample extends JFrame {
         setSize(600, 600);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ArrayList<String> allLooks = new ArrayList<String>();
+        UIManager.LookAndFeelInfo[] looks = UIManager.getInstalledLookAndFeels();
+        for (UIManager.LookAndFeelInfo look : looks){
+            System.out.println(look.getClassName());
+            allLooks.add(look.getClassName());
+        }
+        try {
+        UIManager.setLookAndFeel(allLooks.get(2));
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
