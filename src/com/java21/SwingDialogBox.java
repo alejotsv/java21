ec6898851452f6058a03584df46f00e2af49c924
Alejandro Salas <asalas.wd@outlook.com>
2020-05-17 12:53:46 -0400

    Set position in the middle of screen via a new method

--- src/com/java21/SwingDialogBox.java
+++ src/com/java21/SwingDialogBox.java
@@ -6,23 +6,26 @@ import java.awt.*;
 public class SwingDialogBox extends JFrame {
 
     public static void main(String[] args) {
-        SwingDialogBox thisBox = new SwingDialogBox();
+        SwingDialogBox thisBox = new SwingDialogBox(600, 600, 1);
 
     }
 
-    public SwingDialogBox(){
+    public SwingDialogBox(int x, int y, int monitor){
         super("This is my new Dialog Box");
-        setSize(600, 600);
-        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        GraphicsDevice[] gds = ge.getScreenDevices();
-        GraphicsConfiguration gc = gds[2].getDefaultConfiguration();
-        Rectangle rect = gc.getBounds();
-        System.out.println(rect);
-        int x = rect.getLocation().x;
-        int y = rect.getLocation().y;
-        this.setLocation(x + 660, 240);
+        setSize(x, y);
+        setPosition(x, y, monitor);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    void setPosition(int x, int y, int monitor){
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice[] gds = ge.getScreenDevices();
+        GraphicsConfiguration gc = gds[monitor].getDefaultConfiguration();
+        Rectangle rect = gc.getBounds();
+        int finalX = rect.getLocation().x + ((int)rect.getWidth() - x)/2;
+        int finalY = rect.getLocation().y + ((int)rect.getHeight() - y)/2;
+        this.setLocation(finalX, finalY);
     }
 
 
