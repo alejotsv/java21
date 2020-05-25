04117d199f34cd64cd90ba45cd234388c6661f17
Alejandro Salas <asalas.wd@outlook.com>
2020-05-24 22:49:52 -0400

    Complete BitSetExample

--- src/com/java21/BitSetExample.java
+++ src/com/java21/BitSetExample.java
@@ -6,7 +6,8 @@ public class BitSetExample {
 
     public static void main(String[] args){
         int[] holidays = { 1, 15, 50, 148, 185, 246, 281, 316, 326, 359 };
-        setHolidays(holidays);
+        BitSet myHolidays = setHolidays(holidays);
+        isHoliday(myHolidays, 50);
 
     }
 
