531d459fc3c4c0f596d9e32ca498a980fcde6c3c
Alejandro Salas <asalas.wd@outlook.com>
2020-05-24 22:02:19 -0400

    Create method to set holiday schedule

--- src/com/java21/BitSetExample.java
+++ src/com/java21/BitSetExample.java
@@ -3,21 +3,17 @@ package com.java21;
 import java.util.BitSet;
 
 public class BitSetExample {
-    public static final int PLAY = 0;
-    public static final int STOP = 1;
-    public static final int PAUSE = 2;
 
     public static void main(String[] args){
-        System.out.println(PAUSE);
-
-        BitSet mySet = new BitSet(3);
-
-        mySet.set(0);
-        mySet.clear(1);
-        mySet.clear(2);
-
-        boolean bool = mySet.get(BitSetExample.PAUSE);
-
-        System.out.println(bool);
+        System.out.println("This will return the holidays");
     }
+
+    BitSet setHolidays(int[] holidays){
+        BitSet holidaySchedule = new BitSet(365);
+        for(int holiday : holidays){
+            holidaySchedule.set(holiday);
+        }
+        return holidaySchedule;
+    }
+
 }
