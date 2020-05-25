c4261024d0f1b128a2e09354ba9119b4d582acd7
Alejandro Salas <asalas.wd@outlook.com>
2020-05-25 19:03:19 -0400

    Complete Stack example

---
+++ src/com/java21/StackExample.java
@@ -0,0 +1,38 @@
+package com.java21;
+
+import java.util.*;
+
+public class StackExample {
+    public static void main(String[] args){
+        Stack stack = new Stack();
+        String[] numbers = { "Zero", "One", "Two", "Three", "Four" };
+        for (String num : numbers){
+            stack.push(num);
+            System.out.println("Adding: " + num);
+        }
+
+        System.out.println("--------------------");
+
+        showStack(stack);
+
+        popped(stack);
+        popped(stack);
+        popped(stack);
+        popped(stack);
+
+    }
+
+    static void showStack(Stack stack){
+        for(Object element : stack){
+            System.out.println(element);
+        }
+    }
+
+    static void popped(Stack stack){
+        Object ob = stack.pop();
+        System.out.println("--------------------");
+        System.out.println("This is what I just popped: " + ob);
+        System.out.println("--------------------");
+        showStack(stack);
+    }
+}
