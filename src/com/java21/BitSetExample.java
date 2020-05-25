40596b9b9188cf412d21dc5a02049ff5606705d1
Alejandro Salas <asalas.wd@outlook.com>
2020-05-24 22:05:28 -0400

    Creaqte method to verify if a day is a holiday

--- src/com/java21/BitSetExample.java
+++ src/com/java21/BitSetExample.java
@@ -5,15 +5,21 @@ import java.util.BitSet;
 public class BitSetExample {
 
     public static void main(String[] args){
-        System.out.println("This will return the holidays");
+        int[] holidays = { 1, 15, 50, 148, 185, 246, 281, 316, 326, 359 };
+        setHolidays(holidays);
+
     }
 
-    BitSet setHolidays(int[] holidays){
+    static BitSet setHolidays(int[] holidays){
         BitSet holidaySchedule = new BitSet(365);
         for(int holiday : holidays){
             holidaySchedule.set(holiday);
         }
         return holidaySchedule;
+    }
+
+    static boolean isHoliday(BitSet holidaySet, int day){
+        return holidaySet.get(day);
     }
 
 }
