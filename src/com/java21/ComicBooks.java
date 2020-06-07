947b984e50cc24a5a8c651111c4882a640b381c0
Alejandro Salas <asalas.wd@outlook.com>
2020-06-07 11:11:50 -0400

    Create ComicBooks class

---
+++ src/com/java21/ComicBooks.java
@@ -0,0 +1,24 @@
+package com.java21;
+
+import java.util.HashMap;
+
+public class ComicBooks {
+
+    public static void main(String[] args){
+        System.out.println("This is my Comics class: ");
+    }
+
+    public class Comic {
+        HashMap<String, Float> condition;
+        String title;
+        String issue;
+        float basePrice;
+        float price;
+
+        
+
+    }
+
+
+
+}
