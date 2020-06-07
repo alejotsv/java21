97abb714c8b16c7665e15c1e36c7310931cc53b7
Alejandro Salas <asalas.wd@outlook.com>
2020-06-07 11:39:25 -0400

    Add setInitialCondition and setCondition methods

--- src/com/java21/ComicBooks.java
+++ src/com/java21/ComicBooks.java
@@ -1,29 +1,46 @@
 package com.java21;
 
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class ComicBooks {
+    HashMap<String, Float> condition = new HashMap<String, Float>();
+
+    public ComicBooks() {
+        setInitialCondition();
+    }
 
     public static void main(String[] args){
         System.out.println("This is my Comics class: ");
     }
 
-    public class Comic {
-        HashMap<String, Float> condition = new HashMap<String, Float>();
+    public void setInitialCondition() {
+        String[] conditions = {"mint", "near mint", "very fine", "fine", "good", "poor"};
+        for (String condition : conditions) {
+            this.condition.put(condition, 1F);
+        }
+    }
+
+    public void setCondition(Float[] priceChange){
+        this.condition.replace("mint", priceChange[0]);
+        this.condition.replace("near mint", priceChange[1]);
+        this.condition.replace("very fine", priceChange[2]);
+        this.condition.replace("fine", priceChange[3]);
+        this.condition.replace("good", priceChange[4]);
+        this.condition.replace("poor", priceChange[5]);
+    }
+
+    class Comic {
         String title;
         String issue;
         float basePrice;
         float price;
 
-        public void setInitialCondition(){
-            String[] conditions = { "mint", "near mint", "very fine", "fine", "good", "poor" };
-            for ( String condition : conditions ){
-                this.condition.put(condition, 1F);
-            }
+        Comic(){
+
         }
 
+
     }
-
-
 
 }
