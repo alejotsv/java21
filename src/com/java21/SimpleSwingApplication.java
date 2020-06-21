ec8b7e6a9f791c2df17a942a6f4ba06d41d1fbf9
Alejandro Salas <asalas.wd@outlook.com>
2020-06-21 13:30:28 -0400

    Create Simple Swing Application class

---
+++ src/com/java21/SimpleSwingApplication.java
@@ -0,0 +1,9 @@
+package com.java21;
+import javax.swing.*;
+import java.awt.*;
+
+public class SimpleSwingApplication {
+    public static void main(String[] args) {
+        System.out.println("This is my simple Swing application");
+    }
+}
