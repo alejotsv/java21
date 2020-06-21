e0f4f3019d5d79cb734d9f4fb7b269dd3174940a
Alejandro Salas <asalas.wd@outlook.com>
2020-06-21 13:46:42 -0400

    Have SimpleSwingApplication extend JFrame

--- src/com/java21/SimpleSwingApplication.java
+++ src/com/java21/SimpleSwingApplication.java
@@ -2,7 +2,7 @@ package com.java21;
 import javax.swing.*;
 import java.awt.*;
 
-public class SimpleSwingApplication {
+public class SimpleSwingApplication extends JFrame {
     public static void main(String[] args) {
         System.out.println("This is my simple Swing application");
     }
