0a1f9387d506c354bd0d2f2a45c7e538cf3e4249
Alejandro Salas <asalas.wd@outlook.com>
2020-06-21 14:01:30 -0400

    Add constructor to Simple Swing Example

--- src/com/java21/SimpleSwingApplication.java
+++ src/com/java21/SimpleSwingApplication.java
@@ -5,5 +5,12 @@ import java.awt.*;
 public class SimpleSwingApplication extends JFrame {
     public static void main(String[] args) {
         System.out.println("This is my simple Swing application");
+        SimpleSwingApplication myApp = new SimpleSwingApplication("My App");
+    }
+
+    public SimpleSwingApplication(String title){
+        super(title);
+        setSize(300, 300);
+        setVisible(true);
     }
 }
