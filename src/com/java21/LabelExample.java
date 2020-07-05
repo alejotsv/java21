49b25f300ed78aa8c2ac4d62ef17c9850cb88fdd
Alejandro Salas <asalas.wd@outlook.com>
2020-07-04 21:33:15 -0400

    Add panel

--- src/com/java21/LabelExample.java
+++ src/com/java21/LabelExample.java
@@ -1,7 +1,23 @@
 package com.java21;
 
-public class LabelExample {
+import javax.swing.*;
+import java.awt.*;
+
+public class LabelExample extends JFrame{
+    JPanel panel = new JPanel();
+
+    public LabelExample(String title, Dimension dimensions){
+        super(title);
+        setSize(dimensions);
+
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    }
+
     public static void main(String[] args){
         System.out.println("This is my example for labels");
+        Dimension dim = new Dimension(150, 150);
+        LabelExample newLabel = new LabelExample("Labels", dim);
     }
 }
