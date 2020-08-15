740b47990b2d859624186ccac4843fe1042991c0
Alejandro Salas <asalas.wd@outlook.com>
2020-08-14 21:07:49 -0400

    Add pane, dimensions, and set to visible

--- src/com/java21/TextPasswordField.java
+++ src/com/java21/TextPasswordField.java
@@ -1,4 +1,18 @@
 package com.java21;
 
-public class TextPasswordField {
+import javax.swing.*;
+
+public class TextPasswordField extends JFrame {
+    JPanel pane = new JPanel();
+
+    public TextPasswordField(){
+        super("Password field");
+        setSize(240, 240);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        TextPasswordField passwordField = new TextPasswordField();
+    }
 }
