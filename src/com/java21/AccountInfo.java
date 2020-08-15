35eb82612be00fc1021467aac92704867a3a2e58
Alejandro Salas <asalas.wd@outlook.com>
2020-08-14 21:51:58 -0400

    Add labels, fields, and buttons

--- src/com/java21/AccountInfo.java
+++ src/com/java21/AccountInfo.java
@@ -7,8 +7,30 @@ public class AccountInfo extends JFrame {
 
     public AccountInfo(){
         super("Account Information");
-        setSize(300, 300);
-
+        setSize(300, 180);
+//        Create labels
+        JLabel usernameLabel = new JLabel("Username:");
+        JLabel passwordLabel = new JLabel("Password:");
+        JLabel commentsLabel = new JLabel("Comments:");
+//        Create fields
+        JTextField username = new JTextField(15);
+        JPasswordField password = new JPasswordField(15);
+        password.setEchoChar('*');
+        JTextArea comments = new JTextArea(4,15);
+//        Create buttons
+        JButton ok = new JButton("OK");
+        JButton cancel = new JButton("Cancel");
+//        Add labels, fields, and buttons to pane
+        pane.add(usernameLabel);
+        pane.add(username);
+        pane.add(passwordLabel);
+        pane.add(password);
+        pane.add(commentsLabel);
+        pane.add(comments);
+        pane.add(ok);
+        pane.add(cancel);
+//        Add pane
+        add(pane);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
