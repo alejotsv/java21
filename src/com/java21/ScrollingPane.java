964a8847a870f050ad784d2badde873160b5127a
Alejandro Salas <asalas.wd@outlook.com>
2020-08-17 20:58:19 -0400

    Create ScrollingPane main method

--- src/com/java21/ScrollingPane.java
+++ src/com/java21/ScrollingPane.java
@@ -3,7 +3,15 @@ package com.java21;
 import javax.swing.*;
 
 public class ScrollingPane extends JFrame {
+    JPanel pane = new JPanel();
+
+    public ScrollingPane(){
+        super("Scrolling Pane Example");
+        setSize(300, 300);
+        setVisible(true);
+    }
+
     public static void main(String[] args){
-        System.out.println("Hello scrolling pane");
+        ScrollingPane scroll = new ScrollingPane();
     }
 }
