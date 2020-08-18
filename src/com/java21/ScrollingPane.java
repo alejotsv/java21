7a9322cbaf5157e593dcfc7ac9f6ae036b035e71
Alejandro Salas <asalas.wd@outlook.com>
2020-08-17 21:02:54 -0400

    Add username and password labels and fields

--- src/com/java21/ScrollingPane.java
+++ src/com/java21/ScrollingPane.java
@@ -8,7 +8,19 @@ public class ScrollingPane extends JFrame {
     public ScrollingPane(){
         super("Scrolling Pane Example");
         setSize(300, 300);
+        JLabel username = new JLabel("Username:");
+        JTextField usernameField = new JTextField(15);
+        JLabel password = new JLabel("Password:");
+        JPasswordField passwordField = new JPasswordField(15);
+        passwordField.setEchoChar('*');
+
+        pane.add(username);
+        pane.add(usernameField);
+        pane.add(password);
+        pane.add(passwordField);
+        add(pane);
         setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args){
