329a6a524e291186e95317f323fe32c5bd77f1a4
Alejandro Salas <asalas.wd@outlook.com>
2020-08-17 21:08:40 -0400

    Add scroll bars

--- src/com/java21/ScrollingPane.java
+++ src/com/java21/ScrollingPane.java
@@ -13,11 +13,18 @@ public class ScrollingPane extends JFrame {
         JLabel password = new JLabel("Password:");
         JPasswordField passwordField = new JPasswordField(15);
         passwordField.setEchoChar('*');
-
+        JLabel comments = new JLabel("Comments:");
+        JTextArea commentsArea = new JTextArea(10, 15);
+//        Add Scrolling pane
+        JScrollPane scrollPane = new JScrollPane(commentsArea,
+                ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
+                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         pane.add(username);
         pane.add(usernameField);
         pane.add(password);
         pane.add(passwordField);
+        pane.add(comments);
+        pane.add(scrollPane);
         add(pane);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
