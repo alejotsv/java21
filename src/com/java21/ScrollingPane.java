4206250a9ab2d0ec9c62bb1907669e683dd02697
Alejandro Salas <asalas.wd@outlook.com>
2020-08-17 20:56:10 -0400

    Create Scrolling Pane Class

---
+++ src/com/java21/ScrollingPane.java
@@ -0,0 +1,9 @@
+package com.java21;
+
+import javax.swing.*;
+
+public class ScrollingPane extends JFrame {
+    public static void main(String[] args){
+        System.out.println("Hello scrolling pane");
+    }
+}
