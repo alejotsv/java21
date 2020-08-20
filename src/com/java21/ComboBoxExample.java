d1ef23ff0a402757da77a38a83776a6cb5e28c5d
Alejandro Salas <asalas.wd@outlook.com>
2020-08-19 21:18:06 -0400

    Create basic frame

--- src/com/java21/ComboBoxExample.java
+++ src/com/java21/ComboBoxExample.java
@@ -3,7 +3,17 @@ package com.java21;
 import javax.swing.*;
 
 public class ComboBoxExample extends JFrame {
+    JPanel pane = new JPanel();
+
+    public ComboBoxExample(String[] options){
+        super("Color selection");
+        setSize(500, 500);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
     public static void main(String[] args){
-        System.out.println("Combo example");
+        String[] options = { "Blue", "Orange", "Yellow", "Gray" };
+        ComboBoxExample combo = new ComboBoxExample(options);
     }
 }
