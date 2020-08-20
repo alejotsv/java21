e81b91f74ec4bc0ea18469137b0a1c6be901d1cb
Alejandro Salas <asalas.wd@outlook.com>
2020-08-19 21:04:03 -0400

    Create class to recreate radio buttons from java21

---
+++ src/com/java21/RadioButtonExcercise.java
@@ -0,0 +1,11 @@
+package com.java21;
+
+import javax.swing.*;
+
+public class RadioButtonExcercise extends JFrame {
+
+
+    public static void main(String[] args){
+        System.out.println("Hello excercise");
+    }
+}
