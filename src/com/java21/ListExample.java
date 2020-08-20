f3a2b40192c73a5d88f72a46f09e897ceeee326e
Alejandro Salas <asalas.wd@outlook.com>
2020-08-20 19:53:42 -0400

    Complete exercise

--- src/com/java21/ListExample.java
+++ src/com/java21/ListExample.java
@@ -7,8 +7,14 @@ public class ListExample extends JFrame {
 
     public ListExample(String[] arr){
         super("RSS Subscriptions");
-        setSize(300, 700);
-
+        setSize(200, 300);
+        JList<String> list = new JList<>(arr);
+        list.setVisibleRowCount(4);
+        JScrollPane scroll = new JScrollPane(list, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        JLabel label = new JLabel("RSS Subscriptions");
+        pane.add(label);
+        pane.add(scroll);
+        add(pane);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
