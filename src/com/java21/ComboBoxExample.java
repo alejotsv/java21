bf56f2bcde98a5197e8cb3fc3063236ca06cceff
Alejandro Salas <asalas.wd@outlook.com>
2020-08-19 21:15:04 -0400

    Create ComboBox example for java21

---
+++ src/com/java21/ComboBoxExample.java
@@ -0,0 +1,9 @@
+package com.java21;
+
+import javax.swing.*;
+
+public class ComboBoxExample extends JFrame {
+    public static void main(String[] args){
+        System.out.println("Combo example");
+    }
+}
