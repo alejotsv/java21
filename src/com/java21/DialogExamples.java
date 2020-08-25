1c792cff1c609bbb063cbde482e3b2bd859ee078
Alejandro Salas <asalas.wd@outlook.com>
2020-08-24 21:23:15 -0400

    Create error message dialog box

--- src/com/java21/DialogExamples.java
+++ src/com/java21/DialogExamples.java
@@ -1,9 +1,20 @@
 package com.java21;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class DialogExamples extends JFrame {
+
+    public DialogExamples(){
+        super();
+        int response = JOptionPane.showConfirmDialog(null,
+                "There was an error",
+                "File error",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.ERROR_MESSAGE);
+    }
+
     public static void main(String[] args){
-        System.out.println("Hello dialogs");
+        DialogExamples box = new DialogExamples();
     }
 }
