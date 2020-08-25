d7c11860a81b5c89022b857b9b38f52c5b26fcf7
Alejandro Salas <asalas.wd@outlook.com>
2020-08-24 21:18:38 -0400

    Create class to practice dialog boxes

---
+++ src/com/java21/DialogExamples.java
@@ -0,0 +1,9 @@
+package com.java21;
+
+import javax.swing.*;
+
+public class DialogExamples extends JFrame {
+    public static void main(String[] args){
+        System.out.println("Hello dialogs");
+    }
+}
