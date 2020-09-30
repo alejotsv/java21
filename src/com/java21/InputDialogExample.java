227959a49e7e24e12c4f812774a168fd40318cbd
Alejandro Salas <asalas.wd@outlook.com>
2020-09-30 19:54:07 -0400

    Add message greeting box

--- src/com/java21/InputDialogExample.java
+++ src/com/java21/InputDialogExample.java
@@ -7,8 +7,17 @@ public class InputDialogExample extends JFrame {
     String response = JOptionPane.showInputDialog(null, "Enter your name",
             "Name box", JOptionPane.QUESTION_MESSAGE);
 
+    public void sayHello(){
+        String hello = "Hello, " + this.response;
+        JOptionPane.showMessageDialog(null,
+                hello,
+                "Greetings",
+                JOptionPane.PLAIN_MESSAGE);
+    }
+
+
     public static void main(String[] arr){
         InputDialogExample inputExample = new InputDialogExample();
-        System.out.println("Your name is: " + inputExample.response);
+        inputExample.sayHello();
     }
 }
