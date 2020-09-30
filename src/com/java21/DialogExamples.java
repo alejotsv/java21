2dcf8dd131746843ff1b2a5a486e952ae9fdd1fc
Alejandro Salas <asalas.wd@outlook.com>
2020-09-30 19:39:42 -0400

    Update to Cancel option

--- src/com/java21/DialogExamples.java
+++ src/com/java21/DialogExamples.java
@@ -10,8 +10,9 @@ public class DialogExamples extends JFrame {
         int response = JOptionPane.showConfirmDialog(null,
                 "There was an error",
                 "File error",
-                JOptionPane.YES_NO_OPTION,
+                JOptionPane.YES_NO_CANCEL_OPTION,
                 JOptionPane.ERROR_MESSAGE);
+        System.out.println(response);
     }
 
     public static void main(String[] args){
