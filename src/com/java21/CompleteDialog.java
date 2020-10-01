ba1c33269668ceb54d73f048d0285c396b956b74
Alejandro Salas <asalas.wd@outlook.com>
2020-10-01 12:35:44 -0400

    Start method to create labels

--- src/com/java21/CompleteDialog.java
+++ src/com/java21/CompleteDialog.java
@@ -4,12 +4,21 @@ import javax.swing.*;
 
 public class CompleteDialog extends JFrame {
     JPanel pane = new JPanel();
+    String[] defaultLabels = { "Name: ", "URL: ", "Type: " };
 
     public CompleteDialog(){
         super("This is my box");
         setSize(300,300);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        createLabels(defaultLabels);
+    }
+
+    public void createLabels(String[] labels){
+        for( String label : labels ){
+            System.out.println(label);
+        }
     }
 
     public static void main(String[] args) {
