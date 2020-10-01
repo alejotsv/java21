27fdd914e0a2ba697eceefa028d9f48b1a41e25d
Alejandro Salas <asalas.wd@outlook.com>
2020-10-01 12:32:19 -0400

    Create box

--- src/com/java21/CompleteDialog.java
+++ src/com/java21/CompleteDialog.java
@@ -1,7 +1,18 @@
 package com.java21;
 
-public class CompleteDialog {
+import javax.swing.*;
+
+public class CompleteDialog extends JFrame {
+    JPanel pane = new JPanel();
+
+    public CompleteDialog(){
+        super("This is my box");
+        setSize(300,300);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
     public static void main(String[] args) {
-        System.out.println("This is my game");
+        CompleteDialog myBox = new CompleteDialog();
     }
 }
