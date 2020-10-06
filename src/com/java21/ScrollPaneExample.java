7a9d045adcbd92dde68e1962c678a34529377f64
Alejandro Salas <asalas.wd@outlook.com>
2020-10-06 19:07:35 -0400

    Extend JFrame

--- src/com/java21/ScrollPaneExample.java
+++ src/com/java21/ScrollPaneExample.java
@@ -1,6 +1,8 @@
 package com.java21;
 
-public class ScrollPaneExample {
+import javax.swing.*;
+
+public class ScrollPaneExample extends JFrame {
     public static void main(String[] args) {
         System.out.println("This will scroll");
     }
