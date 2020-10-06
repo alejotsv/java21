c63e9d72e09c6ab183192934628d2b082ac5aea2
Alejandro Salas <asalas.wd@outlook.com>
2020-10-06 18:58:52 -0400

    Complete slider

--- src/com/java21/SliderExample.java
+++ src/com/java21/SliderExample.java
@@ -6,8 +6,12 @@ public class SliderExample extends JFrame {
     JPanel pane = new JPanel();
     public SliderExample(){
         super("This new slider");
+        setSize(450,450);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        JSlider slider = new JSlider();
+        JSlider slider = new JSlider(0, 50, 49);
+        pane.add(slider);
+        add(pane);
+        setVisible(true);
     }
 
     public static void main(String[] args){
