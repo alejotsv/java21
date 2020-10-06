21cc8e982d703c8380c7771c15e4b28ab29c3dee
Alejandro Salas <asalas.wd@outlook.com>
2020-10-06 19:16:51 -0400

    Create and prepare scroll example

--- src/com/java21/ScrollPaneExample.java
+++ src/com/java21/ScrollPaneExample.java
@@ -1,9 +1,21 @@
 package com.java21;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ScrollPaneExample extends JFrame {
+    JPanel pane = new JPanel();
+    Dimension dim = new Dimension(400,400);
+
+    public ScrollPaneExample(){
+        super("This is the scroll example");
+        setSize(this.dim);
+
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
     public static void main(String[] args) {
-        System.out.println("This will scroll");
+        ScrollPaneExample scroller = new ScrollPaneExample();
     }
 }
