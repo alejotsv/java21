6e1d6e8e8e4afbd38e5ce77f40074e600ec8aebd
Alejandro Salas <asalas.wd@outlook.com>
2020-10-06 20:26:16 -0400

    Add icons

--- src/com/java21/ToolBarExample.java
+++ src/com/java21/ToolBarExample.java
@@ -1,7 +1,26 @@
 package com.java21;
 
-public class ToolBarExample {
+import javax.swing.*;
+
+public class ToolBarExample extends JFrame {
+    JPanel pane = new JPanel();
+    String path = "/home/alejotsv/code/java/java21/global21/src/com/java21/img/";
+
+    public ToolBarExample(){
+        super("This is a ToolBar");
+        setSize(400,400);
+
+        ImageIcon downloadIcon = new ImageIcon(this.path + "save.png");
+        JButton downloadButton = new JButton("Download", downloadIcon);
+
+        pane.add(downloadButton);
+        add(pane);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+
+    }
+
     public static void main(String[] args){
-        System.out.println("This is the tool bar");
+        ToolBarExample toolBar = new ToolBarExample();
     }
 }
