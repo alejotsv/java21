75240732f60671bdb599f4546803cad0e8c74f68
Alejandro Salas <asalas.wd@outlook.com>
2020-10-15 19:40:36 -0400

    Complete progress bar

--- src/com/java21/ProgressBarExample.java
+++ src/com/java21/ProgressBarExample.java
@@ -4,16 +4,15 @@ import javax.swing.*;
 
 public class ProgressBarExample extends JFrame {
     JPanel pane = new JPanel();
-    JProgressBar pBar = new JProgressBar(1,100);
+    JProgressBar pBar = new JProgressBar();
 
     public ProgressBarExample(){
         super("Progress Bar");
         setSize(250, 60);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.pBar.setStringPainted(true);
         pane.add(this.pBar);
-
         add(pane);
-
         setVisible(true);
 
         countNumbers(45);
@@ -22,11 +21,14 @@ public class ProgressBarExample extends JFrame {
     }
 
     void countNumbers(int num){
+        this.pBar.setMinimum(0);
+        this.pBar.setMaximum(num);
         int i = 0;
         while(i <= num){
+            this.pBar.setValue(i);
             System.out.println(i);
             try {
-                Thread.sleep(4000);
+                Thread.sleep(1000);
             } catch (InterruptedException e){
                 System.out.println(e);
             }
