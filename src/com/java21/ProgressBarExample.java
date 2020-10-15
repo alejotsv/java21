5de78dfa959a7c5f12c685bcb8ba919c19a79350
Alejandro Salas <asalas.wd@outlook.com>
2020-10-15 19:23:40 -0400

    Add number counter

--- src/com/java21/ProgressBarExample.java
+++ src/com/java21/ProgressBarExample.java
@@ -8,10 +8,17 @@ public class ProgressBarExample extends JFrame {
     public ProgressBarExample(){
         super("Progress Bar");
         System.out.println("This is a progress bar");
-
+        countNumbers(45);
 
     }
 
+    void countNumbers(int num){
+        int i = 0;
+        while(i <= num){
+            System.out.println(i);
+            i++;
+        }
+    }
 
     public static void main(String[] args){
         ProgressBarExample bar = new ProgressBarExample();
