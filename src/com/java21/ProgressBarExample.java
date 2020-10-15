9964a249fb6055c11d622854259427439f80eca1
Alejandro Salas <asalas.wd@outlook.com>
2020-10-15 19:17:11 -0400

    Create JPanel and add title

--- src/com/java21/ProgressBarExample.java
+++ src/com/java21/ProgressBarExample.java
@@ -1,7 +1,19 @@
 package com.java21;
 
-public class ProgressBarExample {
+import javax.swing.*;
+
+public class ProgressBarExample extends JFrame {
+    JPanel pane = new JPanel();
+
+    public ProgressBarExample(){
+        super("Progress Bar");
+        System.out.println("This is a progress bar");
+
+
+    }
+
+
     public static void main(String[] args){
-        System.out.println("This will be a Progress Bar");
+        ProgressBarExample bar = new ProgressBarExample();
     }
 }
