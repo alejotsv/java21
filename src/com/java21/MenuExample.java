0f25022b1a0cbf0f80e9aa4a80ba8535c499efe9
Alejandro Salas <asalas.wd@outlook.com>
2020-10-19 20:34:35 -0400

    Create Menu example class

---
+++ src/com/java21/MenuExample.java
@@ -0,0 +1,9 @@
+package com.java21;
+
+import javax.swing.*;
+
+public class MenuExample extends JFrame {
+    public static void main(String[] args) {
+        System.out.println("My Menu");
+    }
+}
