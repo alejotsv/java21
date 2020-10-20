4a8926c9775267ad7401b6eac65090a5e5b67b3f
Alejandro Salas <asalas.wd@outlook.com>
2020-10-19 20:36:29 -0400

    Add pane and make visible

--- src/com/java21/MenuExample.java
+++ src/com/java21/MenuExample.java
@@ -3,7 +3,17 @@ package com.java21;
 import javax.swing.*;
 
 public class MenuExample extends JFrame {
+    JPanel pane = new JPanel();
+
+    public MenuExample(){
+        super("Menu example");
+        setSize(300, 300);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setVisible(true);
+    }
+
     public static void main(String[] args) {
-        System.out.println("My Menu");
+        MenuExample menu = new MenuExample();
     }
 }
