44d19d2d3a50f1eaabe252616da260154b41881d
Alejandro Salas <asalas.wd@outlook.com>
2020-10-19 20:40:24 -0400

    Create method to create menu items

--- src/com/java21/MenuExample.java
+++ src/com/java21/MenuExample.java
@@ -4,13 +4,23 @@ import javax.swing.*;
 
 public class MenuExample extends JFrame {
     JPanel pane = new JPanel();
+    String[] items = { "Real Madrid", "AC Milan", "River Plate" };
 
     public MenuExample(){
         super("Menu example");
         setSize(300, 300);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        JMenuBar menuBar = new JMenuBar();
+        for ( String item : this.items ){
+            createMenuItem(item);
+        }
+
         setVisible(true);
+    }
+
+    public JMenuItem createMenuItem(String string){
+        return new JMenuItem(string);
     }
 
     public static void main(String[] args) {
