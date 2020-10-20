ae87dcc625bedef6f2983747fbb524df03d7aa59
Alejandro Salas <asalas.wd@outlook.com>
2020-10-19 22:15:32 -0400

    Complete panes

--- src/com/java21/TabbedPanesExample.java
+++ src/com/java21/TabbedPanesExample.java
@@ -3,14 +3,29 @@ package com.java21;
 import javax.swing.*;
 
 public class TabbedPanesExample extends JFrame {
-    JPanel pane = new JPanel();
+
+    String[] tabs = { "Real Madrid", "River Plate", "AC Milan"};
 
     public TabbedPanesExample(){
         super("Tabbed Panes");
         setSize(500, 300);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        JTabbedPane tPane = new JTabbedPane();
+
+        createTabs(this.tabs, tPane);
+
+
+        add(tPane);
         setVisible(true);
+    }
+
+    void createTabs(String[] arr, JTabbedPane pane){
+        for ( String string : arr ){
+            JPanel temp = new JPanel();
+            pane.addTab(string, temp);
+        }
+
     }
 
 
