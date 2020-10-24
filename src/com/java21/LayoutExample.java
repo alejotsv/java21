9b43805551a6d79c90f0a87910ee47257869c86a
Alejandro Salas <asalas.wd@outlook.com>
2020-10-24 09:45:05 -0400

    Set flow and left alignment

--- src/com/java21/LayoutExample.java
+++ src/com/java21/LayoutExample.java
@@ -1,4 +1,26 @@
 package com.java21;
 
-public class LayoutExample {
+import javax.swing.*;
+import java.awt.*;
+
+public class LayoutExample extends JFrame {
+
+    public LayoutExample(){
+        super("This flow is here");
+        setSize(300,300);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        FlowLayout flow = new FlowLayout(FlowLayout.LEFT);
+        setLayout(flow);
+
+        JLabel team = new JLabel("Real Madrid");
+
+        add(team);
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args){
+        System.out.println("This is working");
+        LayoutExample layout = new LayoutExample();
+    }
 }
