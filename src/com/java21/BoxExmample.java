570176d985c9aa3603acfb022fa8a6ca1cde52c2
Alejandro Salas <asalas.wd@outlook.com>
2020-10-24 10:49:27 -0400

    Create box layout example

---
+++ src/com/java21/BoxExmample.java
@@ -0,0 +1,9 @@
+package com.java21;
+
+import javax.swing.*;
+
+public class BoxExmample extends JFrame {
+    public static void main(String[] args) {
+        System.out.println("What's in the box!");
+    }
+}
